73bae96ee42a0ed7831de0a570a3bbac68b3e1cf
st688 <st688@cornell.edu>
2017-01-11 12:01:33 -0500

    fix

--- js/pages/index.js
+++ js/pages/index.js
@@ -1,1 +1,1 @@
-initializeLanguage();loadPages['index'] = true;loadLanguage(userLang);window.onbeforeunload = function(){	loadPages['index'] = false;};
+window.onload = function(){	initializeLanguage();	loadPages['index'] = true;	loadLanguage(userLang);};window.onbeforeunload = function(){	loadPages['index'] = false;};
