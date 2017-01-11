a8943bab9aae52778ad0185cafd202c69cf94c4e
Shih-Hao Tseng <st688@cornell.edu>
2017-01-11 16:34:30 -0500

    add

--- js/pages/index.js
+++ js/pages/index.js
@@ -1,1 +1,1 @@
-window.onload = function(){	initializeLanguage();	loadPages['index'] = true;	loadLanguage(userLang);};window.onbeforeunload = function(){	loadPages['index'] = false;};
+window.onload = function(){	initializeLanguage();	loadPages['index'] = true;	loadContent($('#select-page').val());};window.onbeforeunload = function(){	loadPages['index'] = false;};
