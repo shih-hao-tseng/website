21f778bdb9901285049dfed296d6fb4cb567ea09
Shih-Hao Tseng <shtseng@caltech.edu>
2021-04-11 14:34:36 -0700

    responsive top bar

--- js/index.js
+++ js/index.js
@@ -1,1 +1,14 @@
-window.onload = function(){	initializeLanguage();	loadPages['index'] = true;		loadContent(prevPage);};window.onbeforeunload = function(){	loadPages['index'] = false;};
+window.onload = function(){
+	$('#header-fix').html(document.getElementsByTagName("header")[0].innerHTML);
+	$('#header-fix').each(function (i, e) {
+		$(e).css('display: none;');
+	});
+	initializeLanguage();
+	loadPages['index'] = true;
+	
+	loadContent(prevPage);
+};
+
+window.onbeforeunload = function(){
+	loadPages['index'] = false;
+};
